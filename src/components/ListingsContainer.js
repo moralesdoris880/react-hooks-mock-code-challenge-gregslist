22f8d39f283dafda1f8eba34f0f6d7d4c38fd8aa
Doris Morales <moralesdoris880@gmail.com>
2021-12-16 23:36:13 -0500

    First deliverable done

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
+import ListingCard from "./ListingCard"
 // import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  const [cards,setCards] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:6001/listings")
+    .then((r)=>r.json())
+    .then((cards)=> setCards(cards));
+  },[]);
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+      {cards.map((card)=>{return<ListingCard key={card.id} card={card} />})} 
       </ul>
     </main>
   );
